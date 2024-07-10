e57d259ef8efa8d3d8d92a8ca8b09780c208f00c
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-07-10 01:37:37 -0300

    training camp created

--- src/main/java/com/jessicaoliveira/TesteGoogle.java
+++ src/main/java/com/jessicaoliveira/TesteGoogle.java
@@ -2,6 +2,8 @@ package com.jessicaoliveira;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,7 +16,9 @@ public class TesteGoogle {
         WebDriver driver = new FirefoxDriver();
 //        WebDriver driver = new ChromeDriver();
 
+        driver.manage().window().setSize(new Dimension(1200,765));
         driver.get("https://www.google.com");
         Assert.assertEquals("Google", driver.getTitle());
+        driver.quit();
     }
 }
